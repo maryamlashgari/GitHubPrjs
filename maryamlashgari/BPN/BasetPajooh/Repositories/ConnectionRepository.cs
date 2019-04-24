041e7b0d1314ec7a3a4a46d85186fbc08f6a8a75
Lashgari <Lashgari@DESKTOP-UU1FHJO>
2019-04-24 18:12:04 +0430

    add a repository for connection

---
+++ maryamlashgari/BPN/BasetPajooh/Repositories/ConnectionRepository.cs
@@ -0,0 +1,27 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasetPajooh.Repositories
+{
+    public class ConnectionRepository
+    {
+        public ConnectionRepository(/*AppDbContext context, IDbConnection connection*/)
+        {
+        }
+
+        public List<DeviceAttribute> SelectAllDevices()
+        {
+            DataBaseContext oDbContext = null;
+            oDbContext = new DataBaseContext();
+            DeviceAttribute oDAttribute = new DeviceAttribute();
+            var DeviceAttributes = oDbContext.DeviceAttributes.ToList();
+            return DeviceAttributes;
+            //.Select(p => new { p.ID,p.IP,p.VID,p.PID,p.SerialNum,p.Address })
+        }
+    }
+}
